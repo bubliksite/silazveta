985994d03ce499cf9c56c1ea8bda835e56c83bc3
bubliksite <52692985+bubliksite@users.noreply.github.com>
2020-05-29 00:39:00 +0300

    First section

--- functions.php
+++ functions.php
@@ -1,11 +1,16 @@
-<?
+<?php
 
 /** Перечень стилей и скриптов **/
 function load_style_script () {
-	wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css');
-	wp_enqueue_style('style', get_template_directory_uri() . '/css/style.css');
-	wp_enqueue_style('fonts', get_template_directory_uri() . '/css/fonts.css');
-	wp_enqueue_style('slick', get_template_directory_uri() . '/css/slick.css');
+    wp_enqueue_style('bootstrap.min', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css');
+    wp_enqueue_style('owl.carousel.min', get_template_directory_uri() . '/owlcarousel/owl.carousel.min.css');
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
+
+    wp_enqueue_script('jquery-3.4.1.min', '//code.jquery.com/jquery-3.4.1.min.js');
+    wp_enqueue_script('popper.min', '//cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js');
+    wp_enqueue_script('bootstrap.min', '//stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js');
+    wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js');
+    wp_enqueue_script('owl.carousel.min', get_template_directory_uri() . '/owlcarousel/owl.carousel.min.js');
 }
 
 /** Загрузка стилей и скриптов **/
@@ -16,3 +21,5 @@ function remove_editor() {
   remove_post_type_support('page', 'editor');
 }
 add_action('admin_init', 'remove_editor');
+
+?>
---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <meta property="og:title" content="Стильная кухня от 20 тыс. рублей">
+    <meta property="og:description" content="Ваша кухня устарела и потерлась, а менять ее — дорого? Реновация фасадов — новый тренд в дизайне интерьеров!">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="<?php echo get_template_directory_uri(); ?>/images/icon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;900&display=swap" rel="stylesheet">
+    <title><?php bloginfo('name'); wp_title(); ?></title>
+    <?php wp_head(); ?>
+</head>
+<body>
+    <header class="bg-dark-black">
+        <div class="container d-flex justify-content-between py-3">
+            <div class="logo">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/logo.svg" alt="Логотип">
+            </div>
+            <a class="menu text-decoration-none" href="#calculator">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/icon-calc.svg" alt="Рассчитать стоимость">
+                <span class="ml-2 font-weight-bold text-white text-uppercase" style="font-size: 10px">Рассчитать стоимость</span>
+            </a>
+        </div>
+    </header>
+
